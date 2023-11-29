6d1e42bfbce6b0fc808a7a2441ce0eae72404f82
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-29 21:45:45 +0530

    added:use case 2

--- StackFun.java
+++ StackFun.java
@@ -20,18 +20,32 @@ class StackFun{
 	* @params:Integer data
 	* @return:none
 	*/
-//UC 1
-	void push(int ele) {
-		Node newNode =new Node(ele);
-		if(head ==null) {
-			head=newNode;
-		}
-		else {
-			newNode.next=head;
-			head=newNode;
-		}
-	}
 
+//UC 2
+	void pop() {
+	   if(head==null) {
+		   System.out.println("Empty");
+	   }
+	   else {
+		   System.out.println(head.data+" is popped");
+           head=head.next;
+	   }
+	}
+	
+	/*
+	* @desc:gives the top element
+	* @params:Integer data
+	* @return:none
+	*/
+	void peek() {
+		   if(head==null) {
+			   System.out.println("Empty");
+		   }
+		   else {
+	           System.out.println("top element="+head.data);
+		   }
+		}
+	
 	/*
 	* @desc:display all the elements
 	* @params:Integer data
