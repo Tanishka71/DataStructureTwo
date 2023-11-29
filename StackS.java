8589a5e201655f98c93941f93c5d9bd5b1af7e62
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-29 21:43:52 +0530

    added:use case 1

---
+++ StackS.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.main.queue;
+
+public class StackS {
+	//@desc:main class
+	public static void main(String []args) {
+	StackFun stack=new StackFun();
+	stack.push(70);
+	stack.push(30);
+	stack.push(56);
+	stack.pop();
+	stack.pop();
+	stack.peek();
+	stack.pop();
+    stack.pop();
+	}
+	 
+}
